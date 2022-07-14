78ffa3f2ae18a0fe12d5813803cd619de009fabc
Beksultan <albinblack7@gmail.com>
2022-07-14 13:54:49 +0300

    add MVC pattern with rpn

--- MVC/ViewCOntrollers/ViewController.swift
+++ MVC/ViewCOntrollers/ViewController.swift
@@ -8,127 +8,82 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet var calculatorWorkings: UILabel!
-    @IBOutlet var calculatorResults: UILabel!
-    
-    private var workings = ""
+
+    @IBOutlet weak var currentLabel: UILabel!
+    @IBOutlet weak var resultsLabel: UILabel!
+
+    var calculator: Calculator!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        clearAll()
+        calculator = Calculator()
+        reset()
     }
     
-    
-    //MARK: - IBActions
-    @IBAction func allClearTap(_ sender: UIButton) {
-        clearAll()
-    }
-   
-    @IBAction func backTap(_ sender: UIButton) {
-        if !workings.isEmpty {
-            workings.removeLast()
-            calculatorWorkings.text = workings
-        }
-    }
-    
-    @IBAction func equalsTap(_ sender: UIButton) {
-        if validInput() {
-            let checkWorkingsForPercent = workings.replacingOccurrences(of: "%", with: "*.01")
-            let expression = NSExpression(format: checkWorkingsForPercent)
-            let result = expression.expressionValue(with: nil, context: nil) as! Double
-            let resultString = formatResult(result: result)
-            calculatorResults.text = resultString
+    @IBAction func equalButtonPressed(_ sender: UIButton) {
+        if calculator.validInput() {
+            calculator.equalsPressed()
+            resultsLabel.text = calculator.operationsResult
         } else {
             let alert = UIAlertController(
-                title: "Invalid Input",
-                message: "Calculator unable to do math based of input",
-                preferredStyle: .alert
+                title: "Данные введены не корректно",
+                message: "Калькулятор не может выполнять данное вычисление",
+                preferredStyle: .alert)
+            alert.addAction(UIAlertAction(
+                title: "OK",
+                style: .default)
             )
-            
-            alert.addAction(UIAlertAction(title: "Okay", style: .default))
             self.present(alert, animated: true, completion: nil)
         }
     }
     
-    @IBAction func buttons(_ sender: UIButton) {
-        if sender.tag == 10 {
-            addToWorkings(value: "%")
-        } else if sender.tag == 11 {
-            addToWorkings(value: "/")
-        } else if sender.tag == 12 {
-            addToWorkings(value: "*")
-        } else if sender.tag == 13 {
-            addToWorkings(value: "-")
-        } else if sender.tag == 14 {
-            addToWorkings(value: "+")
-        } else if sender.tag == 15 {
-            addToWorkings(value: ".")
-        }
-    }
-
-    @IBAction func digits(_ sender: UIButton) {
-        addToWorkings(value: String(sender.tag))
-    }
-
-    //MARK: - Private Methods
-    private func clearAll() {
-        workings = ""
-        calculatorWorkings.text = ""
-        calculatorResults.text = ""
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        reset()
     }
     
-    private func specialCharacter(char: Character) -> Bool {
-        if char == "*" {
-            return true
-        } else if char == "/" {
-            return true
-        } else if char == "/" {
-            return true
-        } else if char == "+" {
-            return true
-        }
-        return false
+    @IBAction func deleteButtonPressed(_ sender: UIButton) {
+        calculator.deletePressed()
+        currentLabel.text = calculator.working
     }
-    
-    private func formatResult(result: Double) -> String {
-        if result.truncatingRemainder(dividingBy: 1) == 0 {
-            return String(format: "%.0f", result)
+
+    @IBAction func operationButtonPressed(_ sender: UIButton) {
+        
+        var operation = ""
+
+        let tag = sender.tag
+
+        if tag == 1 {
+            operation = "+"
+        } else if tag == 2 {
+            operation = "-"
+        } else if tag == 3 {
+            operation = "*"
+        } else if tag == 4 {
+            operation = "/"
         } else {
-            return String(format: "%.2f", result)
+            operation = "%"
+        }
+        
+        addToWorkings(value: operation)
+    }
+
+    @IBAction func numberOrDecimalButtonPressed(_ sender: UIButton) {
+        
+        if sender.tag == 10 {
+            addToWorkings(value: ".")
+        } else {
+            addToWorkings(value: String(sender.tag))
         }
     }
     
-    private func validInput() -> Bool {
-        var count = 0
-        var funcCharIndexes = [Int]()
-        
-        for char in workings {
-            if specialCharacter(char: char) {
-                funcCharIndexes.append(count)
-            }
-            count += 1
-        }
-        
-        for index in funcCharIndexes {
-            var previous = -1
-            
-            if index == 0 {
-                return false
-            } else if index == workings.count - 1 {
-                return false
-            } else if previous != 1 {
-                if index - previous == 1 {
-                    return false
-                }
-            }
-            previous = index
-        }
-        return true
+    private func reset() {
+        calculator.clearCurrent()
+        currentLabel.text = ""
+        resultsLabel.text = ""
     }
     
     private func addToWorkings(value: String) {
-        workings = workings + value
-        calculatorWorkings.text = workings
+        calculator.addToWorking(value: value)
+        currentLabel.text = calculator.working
     }
 }
